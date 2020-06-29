7681397f9771d8220702d16c09d110464c87afac
hrl7 <zigen@horol.org>
2020-06-29 22:15:33 +0900

    add tokenizer

---
+++ tokenizer.js
@@ -0,0 +1,71 @@
+const Types = {
+  LPAREN: "LPAREN",
+  RPAREN: "RPAREN",
+  OPERATOR: "OPERATOR",
+  NUMBER: "NUMBER",
+};
+
+function isSpace(c) {
+  return /\s+/.test(c);
+}
+function isDigit(c) {
+  return /\d+/.test(c);
+}
+
+const LParenToken = { type: Types.LPAREN };
+Object.freeze(LParenToken);
+const RParenToken = { type: Types.RPAREN };
+Object.freeze(RParenToken);
+
+const createOperator = (c) => ({ type: Types.OPERATOR, val: c });
+const createNumber = (c) => ({ type: Types.NUMBER, val: +c });
+
+const tokenize = (source) => {
+  let i = 0,
+    c = "";
+  const tokens = [];
+
+  for (i = 0; i < source.length; i++) {
+    c = source[i];
+    if (isSpace(c)) {
+      continue;
+    }
+    let token = null;
+    switch (c) {
+      case "(":
+        token = LParenToken;
+        break;
+      case ")":
+        token = RParenToken;
+        break;
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+      case "=":
+        token = createOperator(c);
+        break;
+      default:
+        break;
+    }
+    if (token) {
+      tokens.push(token);
+      continue;
+    }
+    let buf = "";
+    while (isDigit(c)) {
+      buf += c;
+      c = source[++i];
+    }
+    i--;
+    tokens.push(createNumber(buf));
+  }
+
+  return tokens;
+};
+
+module.exports = {
+  isSpace: isSpace,
+  isDigit: isDigit,
+  tokenize: tokenize,
+};
---
+++ tokenizer.test.js
@@ -0,0 +1,44 @@
+const { tokenize, isSpace, isDigit } = require("./tokenizer");
+
+it("isSpace", () => {
+  expect(isSpace(" ")).toBe(true);
+  expect(isSpace("  ")).toBe(true);
+  expect(isSpace("a")).toBe(false);
+  expect(isSpace("1")).toBe(false);
+  expect(isSpace("-")).toBe(false);
+  expect(isSpace("")).toBe(false);
+});
+
+it("isDigit", () => {
+  expect(isDigit("1")).toBe(true);
+  expect(isDigit("2")).toBe(true);
+  expect(isDigit("0")).toBe(true);
+  expect(isDigit("9")).toBe(true);
+  expect(isDigit("a")).toBe(false);
+  expect(isDigit(" ")).toBe(false);
+});
+
+it("(+ 1 2 3)", () => {
+  expect(tokenize("(+ 1 2 3)")).toEqual([
+    { type: "LPAREN" },
+    { type: "OPERATOR", val: "+" },
+    { type: "NUMBER", val: 1 },
+    { type: "NUMBER", val: 2 },
+    { type: "NUMBER", val: 3 },
+    { type: "RPAREN" },
+  ]);
+});
+
+it("(+ 1 2 3 456 78 9)", () => {
+  expect(tokenize("(+ 1 2 3 456 78 9)")).toEqual([
+    { type: "LPAREN" },
+    { type: "OPERATOR", val: "+" },
+    { type: "NUMBER", val: 1 },
+    { type: "NUMBER", val: 2 },
+    { type: "NUMBER", val: 3 },
+    { type: "NUMBER", val: 456 },
+    { type: "NUMBER", val: 78 },
+    { type: "NUMBER", val: 9 },
+    { type: "RPAREN" },
+  ]);
+});
